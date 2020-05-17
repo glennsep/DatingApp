b8362773bab636261ae2d4134f8646843e06bf94
Glenn Seplowitz <glennsepaws@optonline.net>
2020-05-17 12:57:44 -0400

    Section 8.78

---
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using DatingApp.API.Controllers.DTOs;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>();
+            CreateMap<User, UserForDetailedDto>();
+        }
+    }
+}
