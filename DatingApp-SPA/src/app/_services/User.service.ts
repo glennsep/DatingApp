a35b517df0ea4eb5b74917ac170adba72d39dea0
Glenn Seplowitz <glennsepaws@optonline.net>
2020-06-28 11:16:13 -0400

    Section 16.168

--- DatingApp-SPA/src/app/_services/User.service.ts
+++ DatingApp-SPA/src/app/_services/User.service.ts
@@ -102,7 +102,6 @@ export class UserService {
 
   getMessageThread(id: number, recipientId: number) {
     return this.http.get<Message[]>(
-      this.baseUrl + 'users/' + id + '/messages/thread/' + recipientId
-    );
+      this.baseUrl + 'users/' + id + '/messages/thread/' + recipientId);
   }
 }
